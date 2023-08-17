dfa53c5c4ab6062dda87ad28ff4e8bf1820f7b03
Jubair <jubair.ahmed2838@gmail.com>
2023-08-17 19:23:44 +0600

    overall project done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-ECHO is on.
+// create the onclick function
+let newPrice = 0;
+function addToCard(target) {
+  const selectedFlower = target.parentNode.childNodes[1].innerText,
+        selectedFlowerContainer = document.getElementById("price-section"),
+        li = document.createElement("li"),
+        selectedFlowerPrice = parseInt(target.parentNode.childNodes[5].textContent.split(" ")[1]),
+        currentPrice = document.getElementById('total-price');
+        newPrice = newPrice + selectedFlowerPrice;
+        
+
+  currentPrice.innerText = newPrice;      
+  li.innerText = selectedFlower;
+  selectedFlowerContainer.appendChild(li);
+  
+}
